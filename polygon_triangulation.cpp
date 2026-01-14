fb5a90ff10ee4b7cfddc0518a8eb0da6b114b2a5
Iacopo Moles <iacopo@icpmol.es>
2026-01-14 15:18:15 +0100

    examples of triangulations working

--- polygon_triangulation.cpp
+++ polygon_triangulation.cpp
@@ -1,6 +1,8 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/draw_triangulation_2.h>
+#include <CGAL/draw_point_set_3.h>
+
 #include <CGAL/draw_polygon_2.h>
 #include <CGAL/Polygon_with_holes_2.h>
 #include <CGAL/draw_polygon_with_holes_2.h>
@@ -20,53 +22,121 @@ typedef CGAL::Constrained_triangulation_face_base_2<K>            Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb>               TDS;
 typedef CGAL::Exact_predicates_tag                                Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<K, TDS, Itag>  CDT;
+typedef CGAL::Triangulation_2<K>                                  Triangulation;
 typedef CDT::Face_handle                                          Face_handle;
 typedef CDT::Point                                                Point;
-typedef CGAL::Polygon_2<K>                                        Polygon_2;
-typedef CGAL::Polygon_with_holes_2<K>                             Polygon_with_holes_2;
+typedef CGAL::Polygon_2<K>                                        Polygon;
+// typedef CGAL::Polygon_with_holes_2<K>                             Polygon_with_holes_2;
+
+typedef CGAL::Polygon_with_holes_2<K>                             Polygon_wh;
+typedef std::vector<Point>                                        MultiPoint;
+typedef std::list<Polygon_wh>                                     Poly_list;
+
+void get_obstacles_as_multipoint(MultiPoint& mp)
+{
+  std::ifstream obstacle_file("data/ghiande_obstacles_only_multipart.wkt");
+  std::list<Point> points;
+  int wkt_count = 0;
+  // MultiPoint mp;
+  do
+  {
+    CGAL::IO::read_multi_point_WKT(obstacle_file, mp);
+    // if (wkt_count % 100 == 0)
+    std::cout << "wkt_count multi: " << wkt_count << std::endl;
+    wkt_count++;
+  }while(obstacle_file.good() && !obstacle_file.eof());
+  // CGAL::draw(mp);
+}
+
+void get_perimeter(MultiPoint& perimeter_points)
+{
+  std::ifstream is("data/ghiande_perimeter_2m.wkt");
+  Poly_list polys;
+  int wkt_count = 0;
+  do
+  {
+    Polygon_wh p;
+    CGAL::IO::read_polygon_WKT(is, p);
+    if(!p.outer_boundary().is_empty())
+      polys.push_back(p);
+    std::cout << "wkt_count: " << wkt_count << std::endl;
+    wkt_count++;
+  }while(is.good() && !is.eof());
+
+  for(const Polygon_wh& p_wh : polys)
+  {
+    for (const Point p: p_wh.outer_boundary().vertices())
+      perimeter_points.push_back(p);
+  }
+}
+
+void get_full_field_as_polygon_wh(Polygon_wh& field)
+{
+
+  std::ifstream field_stream("data/ghiande_stems_perimeter.wkt");
+  int wkt_count = 0;
+  do
+  {
+    CGAL::IO::read_polygon_WKT(field_stream, field);
+    // if (wkt_count % 100 == 0)
+    std::cout << "wkt_poly_wh: " << wkt_count << std::endl;
+    wkt_count++;
+  }while(field_stream.good() && !field_stream.eof());
+
+}
 
 int main(int argc, char* argv[])
 {
-  typedef CGAL::Polygon_with_holes_2<K> Polygon;
+
   //std::ifstream is((argc>1)?argv[1]:"data/polygons.wkt");
   //std::list<Polygon> polys;
   // from polygon_wkt
 
-    std::ifstream is("data/ghiande_stems_perimeter.wkt");
-    std::list<Polygon> polys;
-    int wkt_count = 0;
-    do
-    {
-      Polygon p;
-      CGAL::IO::read_polygon_WKT(is, p);
-      if(!p.outer_boundary().is_empty())
-        polys.push_back(p);
-      std::cout << "wkt_count: " << wkt_count << std::endl;
-      wkt_count++;
-    }while(is.good() && !is.eof());
-    for(const Polygon p : polys)
-      CGAL::draw(p);
+
+  MultiPoint perimeter_points;
+  get_perimeter(perimeter_points);
+
+  MultiPoint mp;
+  get_obstacles_as_multipoint(mp);
+  Triangulation tri_ghiande;
+  tri_ghiande.insert(perimeter_points.begin(), perimeter_points.end());
+  tri_ghiande.insert(mp.begin(), mp.end() );
+  std::cout << "display naive triangulation" << std::endl << std::endl;
+  CGAL::draw(tri_ghiande); // naive triangulation
 
 
+  CDT cdt_ghiande;
+  Polygon_wh ghiande_pwh; // ghiande polygon with holes
+  get_full_field_as_polygon_wh(ghiande_pwh);
+  std::cout << "show original field" << std::endl << std::endl;
+
+  CGAL::draw(ghiande_pwh);
+  cdt_ghiande.insert(ghiande_pwh.outer_boundary().begin(), ghiande_pwh.outer_boundary().end());
+  for (const Polygon&  hole: ghiande_pwh.holes())
+    cdt_ghiande.insert_constraint(hole.vertices_begin(), hole.vertices_end(), true);
+
+  std::cout << "constrained triangulation depth: " << cdt_ghiande.dimension() << std::endl;
+  std::cout << "display constrained triangulation" << std::endl << std::endl;
+  CGAL::draw(cdt_ghiande);
 
 
 
   //construct two non-intersecting nested polygons
-  Polygon_2 polygon1;
+  Polygon polygon1;
   polygon1.push_back(Point(10,0));
   polygon1.push_back(Point(12,0));
   polygon1.push_back(Point(12,2));
   polygon1.push_back(Point(11,1.75));
   polygon1.push_back(Point(10,2));
-  Polygon_2 polygon2;
+  Polygon polygon2;
   polygon2.push_back(Point(10.5,0.5));
   polygon2.push_back(Point(11.5,0.5));
   polygon2.push_back(Point(11.5,1.5));
   polygon2.push_back(Point(10.5,1.5));
 
-  std::vector<Polygon_2> holes(1);
+  std::vector<Polygon> holes(1);
   holes[0]=polygon2;
-  Polygon_with_holes_2 pwh(polygon1,holes.begin(),holes.end());
+  Polygon_wh pwh(polygon1,holes.begin(),holes.end());
   //Insert the polygons into a constrained triangulation
   CDT cdt;
   cdt.insert_constraint(polygon1.vertices_begin(), polygon1.vertices_end(), true);
