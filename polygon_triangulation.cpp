7dbdb6adccb797c91af9256e377ce8f6c32c62e0
Iacopo Moles <iacopo@icpmol.es>
2026-01-14 18:46:33 +0100

    logic for obstacles gitignore

--- polygon_triangulation.cpp
+++ polygon_triangulation.cpp
@@ -10,11 +10,16 @@
 #include <CGAL/Polygon_2.h>
 #include <CGAL/IO/WKT.h>
 
+
+#include <CGAL/Polyline_simplification_2/simplify.h>
+#include <CGAL/min_quadrilateral_2.h>
+
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
-
 #include <boost/property_map/property_map.hpp>
+
+namespace PS = CGAL::Polyline_simplification_2;
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel       K;
 typedef CGAL::Triangulation_vertex_base_2<K>                      Vb;
@@ -26,11 +31,18 @@ typedef CGAL::Triangulation_2<K>                                  Triangulation;
 typedef CDT::Face_handle                                          Face_handle;
 typedef CDT::Point                                                Point;
 typedef CGAL::Polygon_2<K>                                        Polygon;
+typedef CGAL::Bbox_2                                              bbox_2;
+
 // typedef CGAL::Polygon_with_holes_2<K>                             Polygon_with_holes_2;
 
 typedef CGAL::Polygon_with_holes_2<K>                             Polygon_wh;
 typedef std::vector<Point>                                        MultiPoint;
 typedef std::list<Polygon_wh>                                     Poly_list;
+
+typedef std::unordered_map<Face_handle, bool>                     FaceOwnershipMap;
+
+typedef PS::Stop_below_count_ratio_threshold                      Stop;
+typedef PS::Squared_distance_cost                                 Cost;
 
 void get_obstacles_as_multipoint(MultiPoint& mp)
 {
@@ -85,6 +97,66 @@ void get_full_field_as_polygon_wh(Polygon_wh& field)
 
 }
 
+void center_coordinates(Polygon_wh& polygon, Polygon_wh& polygon_out)
+{
+  std::list<Polygon> holes_translated;
+  const bbox_2 bounds = polygon.bbox();
+  const double x_min = bounds.xmin();
+  const double y_min = bounds.ymin();
+
+  std::cout << "x_min: " << x_min << "; y_min: " << y_min<< std::endl;
+  CGAL::Aff_transformation_2<K> translate(CGAL::TRANSLATION, CGAL::Vector_2<K>(x_min,y_min));
+
+  Polygon_wh new_polygon(transform(translate,polygon.outer_boundary()));
+  polygon_out = new_polygon;
+  for (const Polygon& hole : polygon.holes())
+    polygon_out.add_hole(transform(translate,hole));
+
+
+}
+
+void simplify_obstacles(Polygon_wh& polygon, Polygon_wh& polygon_out, float width, float cost_stop)
+{
+  Cost cost;
+
+  const Polygon_wh temp(polygon.outer_boundary());
+  polygon_out = temp;
+  for (const Polygon& hole: polygon.holes() )
+  {
+    const bbox_2 bounds = hole.bbox();
+    const double delta_x = bounds.xmax() - bounds.xmin();
+    const double delta_y = bounds.ymax() - bounds.ymin();
+    if ((delta_x>width) ||( delta_y>width))
+    {
+      std::cout << "big hole!!" << std::endl;
+       polygon_out.add_hole(PS::simplify(hole, cost, Stop(cost_stop)));
+
+    } else
+    {
+      Polygon bounding_rectangle;
+      CGAL::min_rectangle_2(
+          hole.vertices_begin(), hole.vertices_end(), std::back_inserter(bounding_rectangle));
+      polygon_out.add_hole(bounding_rectangle);
+    }
+  }
+
+}
+
+void get_stats(const CDT& triangualation, const boost::associative_property_map<FaceOwnershipMap> map)
+{
+  int face_count = 0;
+  int indomain_face_count = 0;
+  for (Face_handle f : triangualation.finite_face_handles())
+  {
+    if ( get(map, f) ) ++indomain_face_count;
+    ++face_count;
+  }
+  std::cout << std::endl << std::endl << "ghiande indomain faces: " << indomain_face_count << std::endl;
+  std::cout << "ghiande total faces: " << face_count << std::endl << std::endl;
+
+
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -106,11 +178,16 @@ int main(int argc, char* argv[])
 
 
   CDT cdt_ghiande;
-  Polygon_wh ghiande_pwh; // ghiande polygon with holes
-  get_full_field_as_polygon_wh(ghiande_pwh);
+  Polygon_wh ghiande_pwh_offset, ghiande_pwh, ghiande_pwh_simple_obstacles; // ghiande polygon with holes
+  get_full_field_as_polygon_wh(ghiande_pwh_offset);
+  center_coordinates(ghiande_pwh_offset, ghiande_pwh);
   std::cout << "show original field" << std::endl << std::endl;
 
   CGAL::draw(ghiande_pwh);
+  simplify_obstacles(ghiande_pwh,ghiande_pwh_simple_obstacles, 1.0, 0.5);
+  CGAL::draw(ghiande_pwh_simple_obstacles);
+
+
 
   // build  constrained triangulation
   cdt_ghiande.insert(ghiande_pwh.outer_boundary().begin(), ghiande_pwh.outer_boundary().end()); // add the boundary
@@ -122,23 +199,24 @@ int main(int argc, char* argv[])
   CGAL::draw(cdt_ghiande);
 
 
-  std::unordered_map<Face_handle, bool> ghiande_in_domain_map;
-  boost::associative_property_map< std::unordered_map<Face_handle,bool> > in_domain_ghiande(ghiande_in_domain_map);
+  FaceOwnershipMap ghiande_in_domain_map;
+  boost::associative_property_map<FaceOwnershipMap> in_domain_ghiande(ghiande_in_domain_map);
 
   CGAL::mark_domain_in_triangulation(cdt_ghiande, in_domain_ghiande);
 
+  get_stats(cdt_ghiande, in_domain_ghiande);
   CGAL::draw(cdt_ghiande, in_domain_ghiande);
 
-  int ghiande_indomain_face_count = 0;
-  int ghiande_face_count = 0;
-
-  for (Face_handle f : cdt_ghiande.finite_face_handles())
-  {
-    if ( get(in_domain_ghiande, f) ) ++ghiande_indomain_face_count;
-    ++ghiande_face_count;
-  }
-  std::cout << std::endl << std::endl << "ghiande indomain faces: " << ghiande_indomain_face_count << std::endl;
-  std::cout << "ghiande total faces: " << ghiande_face_count << std::endl << std::endl;
+  // int ghiande_indomain_face_count = 0;
+  // int ghiande_face_count = 0;
+  //
+  // for (Face_handle f : cdt_ghiande.finite_face_handles())
+  // {
+  //   if ( get(in_domain_ghiande, f) ) ++ghiande_indomain_face_count;
+  //   ++ghiande_face_count;
+  // }
+  // std::cout << std::endl << std::endl << "ghiande indomain faces: " << ghiande_indomain_face_count << std::endl;
+  // std::cout << "ghiande total faces: " << ghiande_face_count << std::endl << std::endl;
 
   return 0;
 
